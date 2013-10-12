ffac6c1c4b049aef9565ec7360c4ba5ec53e69cd
vinod doshi <vinod_doshi@mail.example.com>
2013-10-12 19:21:18 +0530

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,20 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HelloWorld {
+
+	
+	@Test public void testMe()
+	{
+		//return true;
+		Assert.assertTrue(true);
+	}
+	
+    public static void main(String args[])
+    {
+        System.out.println("Hello World!");
+    }
+
+	
+	
+}
